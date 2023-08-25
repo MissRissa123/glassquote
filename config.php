94012fb01e50aecacbdf476164939f40a8d9ddcb
MissRissa123 <138741846+MissRissa123@users.noreply.github.com>
2023-08-25 12:01:53 +1200

    Update config.php

--- config.php
+++ config.php
@@ -3,7 +3,10 @@ return [
     'glassPrices' => [
         '4cl//4cl' => ['classic' => 113.03, 'max' => 170.78, 'xcel' => 178.88],
         '5cl//5cl' => ['classic' => 123.98, 'max' => 182.36, 'xcel' => 200.93],
-    ],
+		'4cltgh//4cl' => ['classic' => 180.00, 'max' => 243.15, 'xcel' => 260.33],
+		'5cltgh//5cl' => ['classic' => 187.53, 'max' => 250.38, 'xcel' => 275.03],
+		'4cltgh//4cltgh' => ['classic' => 247.01, 'max' => 299.60, 'xcel' => 321.00],
+	],
     'wheelsPrice' => 20,
     'stayPrices' => [
         'Awning Sash' => 32,
