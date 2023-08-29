e5f41a7eaa23670f6ff0024fe58d121c0c6829c0
MissRissa123 <138741846+MissRissa123@users.noreply.github.com>
2023-08-29 17:06:44 +1200

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 return [
     'glassPrices' => [
-        '4cl//4cl' => ['classic' => 75.35, 'max' => 113.85, 'xcel' => 119.25],
-        '5cl//5cl' => ['classic' => 82.65, 'max' => 121.57, 'xcel' => 133.95],
-		'4cltgh//4cl' => ['classic' => 120, 'max' => 162.1, 'xcel' => 173.55],
-		'5cltgh//5cl' => ['classic' => 125.02, 'max' => 166.92, 'xcel' => 183.38],
-		'4cltgh//4cltgh' => ['classic' => 164.67, 'max' => 199.73, 'xcel' => 214],
+        '4cl//4cl' => ['classic' => 75.35, 'max' => 113.85, 'xcel' => 119.25, 'sunxgrey' => 119.25],
+        '5cl//5cl' => ['classic' => 82.65, 'max' => 121.57, 'xcel' => 133.95, 'sunxgrey' => 133.95],
+		'4cltgh//4cl' => ['classic' => 120, 'max' => 162.1, 'xcel' => 173.55, 'sunxgrey' => 173.55],
+		'5cltgh//5cl' => ['classic' => 125.02, 'max' => 166.92, 'xcel' => 183.38, 'sunxgrey' => 183.38],
+		'4cltgh//4cltgh' => ['classic' => 164.67, 'max' => 199.73, 'xcel' => 214, 'sunxgrey' => 214],
 	],
     'wheelsPrice' => 20,
     'stayPrices' => [
